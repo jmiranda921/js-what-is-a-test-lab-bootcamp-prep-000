3aa5bce187fa188b180a8883a342c928f94de0d7
Jose Miranda <gringo.rk.8@gmail.com>
2018-04-09 09:37:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,23 +3,23 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
+      expect(index.name).toEqual('Susan');
+    });
   })
-
+;
 
   describe('Height', () => {
     it('is less than 74', () => {
-      expect(index.height).toBeLessThan(74)
-    })
-  })
+      expect(index.height).toBeLessThan(74);
+    });
+  });
 
   describe('Message', () => {
     it('gives the name and height 40', () => {
       expect(index.message).toInclude(index.name);
-      expect(index.message).toInclude(index.height)
-    })
-  })
+      expect(index.message).toInclude(index.height);
+    });
+  });
 
-})
+});
 
